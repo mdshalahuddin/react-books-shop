5e2c3fb6f373a851190fd444feaa43503a598a80
mdshalahuddin <mdshalahuddin004@gmail.com>
2022-04-03 05:19:51 +0600

    react projects

--- src/App.js
+++ src/App.js
@@ -9,6 +9,56 @@ function App() {
     <div className="App">
       <h3>স্টকের প্রোডাক্ট সমূহ</h3>
       <Shop></Shop>
+      <div className="ans">
+        <h3>কিভাবে React.js কাজ করে?</h3>
+        <p> <strong>Ans:</strong> Easy creation of dynamic applications: React makes it easier to create dynamic web applications because it requires less coding and offers more functionality, as opposed to JavaScript, where coding often gets complex very quickly.</p>
+        <h3>Difference Between React Props And  State</h3>
+        <table>
+          <tr>
+            <th>PROPS	</th>
+            <th>STATE</th>
+          </tr>
+          <tr>
+            <td>Is read-only.</td>
+            <td>Can be asynchronous.</td>
+          </tr>
+          <tr>
+            <td>Is immutable.	</td>
+            <td>Is mutable.</td>
+          </tr>
+          <tr>
+            <td>Allow the user to pass data from one component to other components as an argument.</td>
+            <td>Holds information about the components.</td>
+          </tr>
+          <tr>
+            <td>Can be accessed by the child component.	</td>
+            <td>Cannot be accessed by child components.</td>
+          </tr>
+          <tr>
+            <td>Used to communicate between components.	</td>
+            <td>Can be used for rendering dynamic changes with the component.</td>
+          </tr>
+          <tr>
+            <td>Stateless components can have them.	</td>
+            <td>Stateless components can not have them.</td>
+          </tr>
+          <tr>
+            <td>Are external and is controlled by whatever renders the component.	</td>
+            <td>Is internal and is controlled by the React Component itself.</td>
+          </tr>
+          <tr>
+            <td>Can not change inside Component.	</td>
+            <td>Can change inside Component.
+            </td>
+          </tr>
+          <tr>
+            <td>Can change in child Components.	</td>
+            <td>Can not change in child Components.</td>
+          </tr>
+        </table>
+
+        <h3>How does useState work?</h3>
+      </div>
     </div>
   );
 }
