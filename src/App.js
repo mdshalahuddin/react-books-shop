5e19487550b50738d097138c2732df03cc778bc1
mdshalahuddin <mdshalahuddin004@gmail.com>
2022-04-03 05:23:37 +0600

    react text

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ function App() {
         </table>
 
         <h3>How does useState work?</h3>
+        <p><strong>Ans:</strong> useState is a Hook that allows you to have state variables in functional components. You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.</p>
       </div>
     </div>
   );
